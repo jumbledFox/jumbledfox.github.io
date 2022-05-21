de63a5c947fe186805a0ffd13134be004ed52ddc
jumbledFox <81022536+jumbledFox@users.noreply.github.com>
2022-05-21 21:20:47 +0100

    do lots of things

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # jumbledfox.github.io
 This is a temporary place for me to host some rubbish
 (this may be more than temporary)
+
+## I've made (making) this a single page application!
+I've used some code from [here](https://github.com/rafgraph/spa-github-pages), but I haven't copied any!! I've read it through and tried to understand it without directly plagiarising anything. Please send me a message if you (yes, you! the random person reading this, quickly leaving after, and getting back to whatever they were trying to do before they stopped for a break. A break, it's always a break. You sit there and try, just **try** to be productive only to get sidetracked on some stupid little thing, it always happens. I dont want this, I don't need this! I just want to be fulfilled, I just need a meaning... I can't take it anymore!! ) find any bugs! 
---
+++ script.js
@@ -0,0 +1,58 @@
+gsap.to("html", {
+    keyframes: {
+        "0%"  : { "--color-rainbow": "69 , 163, 229" },
+        "12%" : { "--color-rainbow": "51 , 204, 204" },
+        "25%" : { "--color-rainbow": "102, 191, 57 " },
+        "38%" : { "--color-rainbow": "255, 166, 2  " },
+        "50%" : { "--color-rainbow": "235, 103, 15 " },
+        "62%" : { "--color-rainbow": "255, 51 , 85 " },
+        "76%" : { "--color-rainbow": "134, 76 , 191" },
+        "100%": { "--color-rainbow": "69 , 163, 229" }
+    },
+    duration: 7,
+    repeat: -1,
+    ease: "none"
+});
+
+$(document).ready(function() {
+    var flyholder = $('#fly-holder');
+
+    var delay = 500,
+        defaultAmount = 13;
+    function makeNewFox() {
+        var
+        scale = "scale(" + rand(0.5, 1) + ")",
+        top = rand(0, 100) + "%",
+        opacity = rand(0.05, 0.1),
+        index = rand(-49, -29),
+        duration = rand(8, 12),
+        fromLeft = Math.round(rand(0, 1));
+        
+        var fox = $("<div></div>").css({
+            "top": top,
+            "opacity": opacity,
+            "transform": scale,
+            "animation-duration": duration - 0 + "s",
+            "z-index": index
+        });
+        fox.addClass("foxhead");
+        fox.addClass(fromLeft ? "foxl" : "foxr");
+
+        flyholder.append(fox);
+
+        setTimeout(function() {
+            makeNewFox();
+            fox.remove();
+        }, duration * 1000);
+    }
+
+    for (var i = 0; i < defaultAmount; i++) {
+        setTimeout(makeNewFox, delay * i);
+    }
+
+});
+
+function rand(min, max)
+{
+	return Math.random() * (max - min) + min;
+}
