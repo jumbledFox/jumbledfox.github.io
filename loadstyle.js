fc3c20d999b92ea071153836cc8cf51bbc54051e
jumbledFox <81022536+jumbledFox@users.noreply.github.com>
2022-07-21 16:26:07 +0100

    Update loadstyle.js

--- loadstyle.js
+++ loadstyle.js
@@ -4,6 +4,13 @@ document.writeln("<link rel='icon' type='image/x-icon' href='images/favicon.png'
 document.writeln("<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js'></script> <!-- GSAP - rainbow -->");
 document.writeln("<script src='https://cdn.jsdelivr.net/remarkable/1.7.1/remarkable.min.js'></script> <!-- Remarkable - markdown -->");
 
+for (let i in ["https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js", "https://cdn.jsdelivr.net/remarkable/1.7.1/remarkable.min.js"]) {
+    var x = document.createElement('script');
+    x.src = i;
+    document.getElementsByTagName("head")[0].appendChild(x);
+}
+
+
 gsap.to("html", {
     keyframes: {
         "0%"  : { "--color-rainbow": "69 , 163, 229" },
