aa3f838b9d6b7c4479e158825b2be81b250bf204
jumbledFox <81022536+jumbledFox@users.noreply.github.com>
2022-07-23 14:32:17 +0100

    Update loadstyle.js

--- loadstyle.js
+++ loadstyle.js
@@ -1,6 +1,6 @@
 // This loads all of the stuff like the style sheet, rainbow text, favicon, gradient, etc.
 document.writeln("<link rel='stylesheet' href='/css/style.css'>");
-document.writeln("<link rel='icon' type='image/x-icon' href='/images/favicon.png' id='favicon'>");
+document.writeln("<link rel='icon' type='image/x-icon' href='' id='favicon'>");
 
 let scripts = ["https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js", "https://cdn.jsdelivr.net/remarkable/1.7.1/remarkable.min.js"];
 for (let i = 0; i < 2; i++) {
@@ -9,24 +9,29 @@ for (let i = 0; i < 2; i++) {
     document.getElementsByTagName("head")[0].appendChild(x);
 }
 
+var setuptimes = 0;
 function setup() {
-    gsap.to("html", {
-        keyframes: {
-            "0%"  : { "--color-rainbow": "69 , 163, 229" },
-            "12%" : { "--color-rainbow": "51 , 204, 204" },
-            "25%" : { "--color-rainbow": "102, 191, 57 " },
-            "38%" : { "--color-rainbow": "255, 166, 2  " },
-            "50%" : { "--color-rainbow": "235, 103, 15 " },
-            "62%" : { "--color-rainbow": "255, 51 , 85 " },
-            "76%" : { "--color-rainbow": "134, 76 , 191" },
-            "100%": { "--color-rainbow": "69 , 163, 229" }
-        },
-        duration: 7,
-        repeat: -1,
-        ease: "none"
-    });
+    
+    if(setuptimes == 0) {
+        gsap.to("html", {
+            keyframes: {
+                "0%"  : { "--color-rainbow": "69 , 163, 229" },
+                "12%" : { "--color-rainbow": "51 , 204, 204" },
+                "25%" : { "--color-rainbow": "102, 191, 57 " },
+                "38%" : { "--color-rainbow": "255, 166, 2  " },
+                "50%" : { "--color-rainbow": "235, 103, 15 " },
+                "62%" : { "--color-rainbow": "255, 51 , 85 " },
+                "76%" : { "--color-rainbow": "134, 76 , 191" },
+               "100%": { "--color-rainbow": "69 , 163, 229" }
+           },
+           duration: 7,
+           repeat: -1,
+           ease: "none"
+        });
 
-    var gradient = document.createElement('div');
-    gradient.id = "gradient";
-    document.body.appendChild(gradient);
+        var gradient = document.createElement('div');
+        gradient.id = "gradient";
+        document.body.appendChild(gradient);
+    }
+    document.getElementById("favicon").href = "/images/favicon.png";
 }
